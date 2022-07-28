781f5e2ac7dbea4daccbf1f26a92507eadb145d1
Kyler Rupe <krupe@catalyte.io>
2022-07-28 12:43:15 -0500

    add jsdocs

--- src/main/java/io/catalyte/training/sportsproducts/domains/product/ProductController.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/product/ProductController.java
@@ -26,6 +26,11 @@ public class ProductController {
   @Autowired
   private ProductService productService;
 
+  /**
+   *  Controller method for getting all products in the database
+   * @param product
+   * @return - a list of products in database and a 200 status
+   */
   @GetMapping
   public ResponseEntity<List<Product>> getProducts(Product product) {
     logger.info("Request received for getProducts");
@@ -33,6 +38,11 @@ public class ProductController {
     return new ResponseEntity<>(productService.getProducts(product), HttpStatus.OK);
   }
 
+  /**
+   *  Controller method for getting a product from database by id
+   * @param id - id to get product by
+   * @return - a product by id and a 200 status
+   */
   @GetMapping(value = "/{id}")
   @ResponseStatus(value = HttpStatus.OK)
   public ResponseEntity<Product> getProductById(@PathVariable Long id) {
@@ -41,7 +51,10 @@ public class ProductController {
     return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
   }
 
-
+  /**
+   *  Controller method for getting a list of types from database
+   * @return - a list of strings with the unique types and a 200 status
+   */
   @GetMapping(value = "/types")
   public ResponseEntity<List<String>> getUniqueType() {
     logger.info("Request received for getUniqueTypes");
@@ -49,6 +62,10 @@ public class ProductController {
     return new ResponseEntity<>(productService.getUniqueTypes(), HttpStatus.OK);
   }
 
+  /**
+   *  Controller method for getting a list of categories from database
+   * @return - a list of strings with the unique categories and a 200 status
+   */
   @GetMapping(value = "/categories")
   public ResponseEntity<List<String>> getUniqueCategory() {
     logger.info("Request received for getUniqueCategory");
